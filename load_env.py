038b68bca606462607ad49222fa4ef756fec70e3
Gloria <gma89@sfu.ca>
2025-03-07 00:47:01 -0800

    Updated .env

--- load_env.py
+++ load_env.py
@@ -7,16 +7,13 @@ def load_environment_variables():
 
     # Access variables
     jupyter_token = os.getenv("JUPYTER_TOKEN")
-    api_key = os.getenv("WEATHER_API_KEY")
+    weather_api_key = os.getenv("WEATHER_API_KEY")
+    traffic_api_key = os.getenv("TRAFFIC_API_KEY")
     kafka_url = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
-
-    # Optional: Print to verify (remove in production)
-    print(f"Jupyter Token: {jupyter_token}")
-    print(f"API Key: {api_key}")
-    print(f"Kafka Broker URL: {kafka_url}")
 
     return {
         "JUPYTER_TOKEN": jupyter_token,
-        "API_KEY": api_key,
+        "WEATHER_API_KEY": weather_api_key,
+        "TRAFFIC_API_KEY": traffic_api_key,
         "KAFKA_BROKER_URL": kafka_url,
     }
